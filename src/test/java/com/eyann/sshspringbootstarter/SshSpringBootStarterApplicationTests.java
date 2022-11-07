f6684027f7da2568ddbaef5f600d03fb5c30195d
Yann Ann <xiaoqiang.yann@gmail.com>
2022-11-07 20:42:31 +0800

    project init

---
+++ src/test/java/com/eyann/sshspringbootstarter/SshSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyann.sshspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SshSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
